f93d522b79f29ec1691b578969acdb2757466ae1
gitusp <ususso@gmail.com>
2023-03-29 17:23:31 +0900

    Fix ctrl-q behavior

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,4 +8,12 @@ config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font('Source Han Code JP', { weight = 'Medium' })
 config.font_size = 11.0
 
+config.keys = {
+  {
+    key = 'q',
+    mods = 'CTRL',
+    action = wezterm.action.SendString '\x11',
+  },
+}
+
 return config
