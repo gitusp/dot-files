fcea606a93d3cef4e294728b251f22b6caac1668
gitusp <ususso@gmail.com>
2023-03-29 15:59:34 +0900

    Set font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,5 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tomorrow'
 config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font('Source Han Code JP', { weight = 'Medium' })
+config.font_size = 11.0
 
 return config
