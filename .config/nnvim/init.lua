98ed32b843fa86886e5726ccbc81e61aab5d80c1
usp <ususso@gmail.com>
2025-01-21 13:36:37 +0900

    Add default settings

--- .config/nnvim/init.lua
+++ .config/nnvim/init.lua
@@ -1,1 +1,18 @@
 require("config.lazy")
+
+vim.o.mouse = ""
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.inccommand = "nosplit"
+vim.o.wildignorecase = true
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smarttab = true
+
+vim.o.lazyredraw = true
+vim.o.shortmess = vim.o.shortmess .. 'I'
+vim.o.showtabline = 0
+vim.o.diffopt = vim.o.diffopt .. ',iwhite'
