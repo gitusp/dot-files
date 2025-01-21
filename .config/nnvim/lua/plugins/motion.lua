b117d3d9673517459ad151fc007d379215f051bb
usp <ususso@gmail.com>
2025-01-21 12:58:15 +0900

    Add motion plugin

---
+++ .config/nnvim/lua/plugins/motion.lua
@@ -0,0 +1,8 @@
+return {
+  { "haya14busa/vim-edgemotion",
+      keys = {
+        { "<c-j>", "<Plug>(edgemotion-j)", mode = { "n", "x" } },
+        { "<c-k>", "<Plug>(edgemotion-k)", mode = { "n", "x" } },
+      },
+  }
+}
