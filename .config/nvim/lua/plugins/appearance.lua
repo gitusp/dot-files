b9b80a43e2511c781db1da6da04010969d2368f5
usp <ususso@gmail.com>
2025-01-28 10:09:36 +0900

    Install tiny-diagnostic

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -15,5 +15,14 @@ return {
       config = function()
         require('lualine').setup()
       end
+  },
+  {
+      "rachartier/tiny-inline-diagnostic.nvim",
+      event = "VeryLazy", -- Or `LspAttach`
+      priority = 1000, -- needs to be loaded in first
+      config = function()
+        require('tiny-inline-diagnostic').setup()
+        vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+      end
   }
 }
