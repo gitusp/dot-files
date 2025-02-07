8950f7a288d81a21a01f78b7cd7be9ed88d9ac20
usp <ususso@gmail.com>
2025-02-07 17:21:10 +0900

    Increase update checker freq

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -31,5 +31,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "github_light" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    frequency = 3600 * 24, -- 1 day
+  },
 })
