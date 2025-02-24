c61208360e8db978018b894d3b71a867ade6f535
usp <ususso@gmail.com>
2025-02-24 09:31:58 +0900

    Enable flash

--- .config/nvim/lua/plugins/motion.lua
+++ .config/nvim/lua/plugins/motion.lua
@@ -5,4 +5,20 @@ return {
         { "<c-k>", "<Plug>(edgemotion-k)", mode = { "n", "x" } },
       },
   },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          enabled = false,
+        }
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    },
+  }
 }
