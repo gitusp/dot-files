0ca7adbacd047e6ecb1ab9e91450956b7a661b78
usp <ususso@gmail.com>
2025-02-25 21:43:06 +0900

    Implement smart indent

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -25,6 +25,25 @@ return {
       vim.api.nvim_call_function("lexima#add_rule", {
         {
           char = '<cr>',
+          input = '<bs><bs>',
+          at = '^- \\%#$',
+          filetype = 'markdown',
+          priority = 1
+        },
+      })
+      vim.api.nvim_call_function("lexima#add_rule", {
+        {
+          char = '<cr>',
+          input = '<bs><bs><bs>- ',
+          at = '^\\s\\+- \\%#$',
+          filetype = 'markdown',
+          priority = 1,
+          with_submatch = true
+        },
+      })
+      vim.api.nvim_call_function("lexima#add_rule", {
+        {
+          char = '<cr>',
           input = '<cr>- ',
           at = '^\\s*- .*\\%#',
           filetype = 'markdown'
