e337d5316444d5f9b2a2469d0b2a620731b959dd
usp <ususso@gmail.com>
2025-02-26 09:42:15 +0900

    Add smart indent for checkbox

--- .config/nvim/lua/plugins/lexima.lua
+++ .config/nvim/lua/plugins/lexima.lua
@@ -26,7 +26,24 @@ return {
           at = '^\\s\\+- \\%#$',
           filetype = 'markdown',
           priority = 1,
-          with_submatch = true
+        },
+      })
+      vim.api.nvim_call_function("lexima#add_rule", {
+        {
+          char = '<cr>',
+          input = '<bs><bs><bs><bs><bs><bs>',
+          at = '^- \\[ ] \\%#$',
+          filetype = 'markdown',
+          priority = 1
+        },
+      })
+      vim.api.nvim_call_function("lexima#add_rule", {
+        {
+          char = '<cr>',
+          input = '<bs><bs><bs><bs><bs><bs><bs>- [ ] ',
+          at = '^\\s\\+- \\[ ] \\%#$',
+          filetype = 'markdown',
+          priority = 1,
         },
       })
       vim.api.nvim_call_function("lexima#add_rule", {
