e342a84a0acfc13af129aa1c95ccf7dbef7dd9bd
usp <ususso@gmail.com>
2025-03-07 10:51:24 +0900

    Revert "Prefer simpler solution"
    This reverts commit 2ab9a841bc29a5156b97a35f38eed18a70a56b29.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,7 +1,14 @@
 return {
   { "tpope/vim-fugitive" },
   { "tpope/vim-rhubarb" },
-  { "junegunn/gv.vim" },
+  {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = {
+      "tpope/vim-fugitive",
+    },
+  },
   { "lewis6991/gitsigns.nvim",
       config = function()
         require("gitsigns").setup({
