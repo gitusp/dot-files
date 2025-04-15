f1c6e0516c9de95eb76d457b09296ddfa9b45507
usp <ususso@gmail.com>
2025-04-15 21:13:23 +0900

    feat!: update wezterm appearance

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,8 +5,10 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tomorrow'
 config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font('Source Han Code JP', { weight = 'Medium' })
-config.font_size = 11.0
+config.font = wezterm.font('PlemolJP Console NF', { weight = 'Medium' })
+config.font_size = 12.0
+config.line_height = 1.1
+config.cell_width = 1.05
 config.initial_rows = 96
 config.initial_cols = 320
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
