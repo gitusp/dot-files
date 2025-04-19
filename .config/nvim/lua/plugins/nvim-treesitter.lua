9c2bf639620e396ff969d19bd40edf9dfdb8dad3
usp <ususso@gmail.com>
2025-04-19 11:52:16 +0900

    feat!: change keymap

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -37,7 +37,7 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = '<CR>',
+            init_selection = "gnn",
             scope_incremental = '<CR>',
             node_incremental = '<TAB>',
             node_decremental = '<S-TAB>',
