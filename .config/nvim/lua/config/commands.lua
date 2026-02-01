a719e450abc879a190bcb96f9f9537ee352c97d9
usp <ususso@gmail.com>
2026-02-01 20:56:54 +0900

    feat!: remove title bar

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,17 +1,9 @@
 vim.api.nvim_create_user_command('DailyScrum', function(opts)
   local days = tonumber(opts.args) or 0
-  local title = vim.fn.strftime("%Y-%m-%d", vim.fn.localtime() + days * 86400)
-
+  local filename = vim.fn.strftime("%Y-%m-%d", vim.fn.localtime() + days * 86400)
   vim.cmd('vnew')
-  vim.cmd('e ~/vaults/scrums/' .. title .. '.md')
+  vim.cmd('e ~/vaults/scrums/' .. filename .. '.md')
   vim.keymap.set("n", "gq", "<cmd>q<cr>", { buffer = true })
-
-  if vim.fn.filereadable(vim.fn.expand('%')) == 0 then
-    local buf = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_lines(buf, 0, 1, false, {title, "===", "", ""})
-    vim.cmd('normal! G')
-    vim.cmd('w')
-  end
 end, { bang = true, nargs = '?' })
 
 vim.api.nvim_create_user_command('Journal', function()
