2e4ad8c96b67d3c32e4f7db87b1f0a89b4d5bb57
usp <ususso@gmail.com>
2026-02-07 18:38:51 +0900

    feat: try codex-acp

---
+++ .config/nvim/lua/plugins/acp.lua
@@ -0,0 +1,22 @@
+return {
+  "carlos-algms/agentic.nvim",
+
+  opts = {
+    provider = "codex-acp",
+  },
+
+  keys = {
+    {
+      "<C-\\>",
+      function() require("agentic").toggle() end,
+      mode = { "n", "v", "i" },
+      desc = "Toggle Agentic Chat"
+    },
+    {
+      "<C-'>",
+      function() require("agentic").add_selection_or_file_to_context() end,
+      mode = { "n", "v" },
+      desc = "Add file or selection to Agentic to Context"
+    },
+  },
+}
