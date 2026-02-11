c91ae2d13311442f7ebfe962f8a452f5a227924d
usp <ususso@gmail.com>
2026-02-11 10:12:50 +0900

    Revert "feat!: use raycast instead"
    This reverts commit d9c7237434b1dc06a735cfc25f90fd3132082741.

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_user_command('DailyScrum', function(opts)
+  local days = tonumber(opts.args) or 0
+  local filename = vim.fn.strftime("%Y-%m-%d", vim.fn.localtime() + days * 86400)
+  vim.cmd('e ~/vaults/scrums/' .. filename .. '.md')
+  vim.keymap.set("n", "gq", "<cmd>q<cr>", { buffer = true })
+end, { bang = true, nargs = '?' })
+
 vim.api.nvim_create_user_command('Journal', function()
   vim.cmd('e ~/vaults/journal/' .. vim.fn.strftime("%Y%m%dT%H%M%S") .. '.md')
   vim.keymap.set("n", "gq", "<cmd>q<cr>", { buffer = true })
