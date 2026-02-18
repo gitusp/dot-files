05e5a7b23f20e70ce5c72686107847cceeb28d95
usp <ususso@gmail.com>
2026-02-18 09:59:51 +0900

    feat: install docbase plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -6,4 +6,29 @@ return {
       vim.g.table_mode_disable_tableize_mappings = 1
     end,
   },
+  {
+    'kyoh86/vim-docbase',
+    init = function()
+      local domain = vim.env.DOCBASE_DOMAIN
+      local token = vim.env.DOCBASE_TOKEN
+
+      if domain ~= nil and domain ~= "" and token ~= nil and token ~= "" then
+        vim.g.docbase = {
+          domains = {
+            {
+              domain = domain,
+              token = token,
+            },
+          },
+        }
+      end
+    end,
+  },
+  {
+    'kyoh86/vim-metarw-docbase',
+    dependencies = {
+      'kana/vim-metarw',
+      'kyoh86/vim-docbase',
+    },
+  },
 }
