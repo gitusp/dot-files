4e5c870073df5705f43f6963a1a69bdb2cdbbf50
usp <ususso@gmail.com>
2026-02-24 08:59:07 +0900

    chore: migrate nvim-surround v3 to v4

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -3,12 +3,5 @@ return {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        keymaps = {
-          visual_line = "<c-s>",
-        }
-      })
-    end
   }
 }
