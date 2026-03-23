51c1cbb1c02352ecf2cd80b2dbd1a75781e926b5
usp <ususso@gmail.com>
2026-03-23 11:50:20 +0900

    feat: remove motion plugins

--- .config/nvim/lua/plugins/motion.lua
+++ .config/nvim/lua/plugins/motion.lua
@@ -1,27 +1,4 @@
 return {
-  { "haya14busa/vim-edgemotion",
-      keys = {
-        { "<c-j>", "<Plug>(edgemotion-j)", mode = { "n", "x" } },
-        { "<c-k>", "<Plug>(edgemotion-k)", mode = { "n", "x" } },
-      },
-  },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {
-      modes = {
-        char = {
-          enabled = false,
-        }
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    },
-  },
   {
     "kana/vim-textobj-entire",
     dependencies = {
