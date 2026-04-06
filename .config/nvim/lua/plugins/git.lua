a3deafb4752dd14c76107604205c2f718684b5ad
usp <ususso@gmail.com>
2026-04-06 09:51:15 +0900

    feat: change graph viewer

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,14 +1,6 @@
 return {
   { "tpope/vim-fugitive" },
   { "tpope/vim-rhubarb" },
-  {
-    "rbong/vim-flog",
-    lazy = true,
-    cmd = { "Flog", "Flogsplit", "Floggit" },
-    dependencies = {
-      "tpope/vim-fugitive",
-    },
-  },
   { "lewis6991/gitsigns.nvim",
       config = function()
         require("gitsigns").setup({
@@ -40,6 +32,51 @@ return {
           end
         })
       end
+  },
+  {
+    "isakbm/gitgraph.nvim",
+    lazy = true,
+    cmd = "GitGraph",
+    opts = {
+      hooks = {
+        on_select_commit = function(commit)
+          vim.cmd(":vert Git show " .. commit.hash)
+        end,
+        on_select_range_commit = function(from, to)
+          vim.cmd(":vert Git diff " .. from.hash .. " " .. to.hash)
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("gitgraph").setup(opts)
+      vim.api.nvim_create_user_command("GitGraph", function()
+        require("gitgraph").draw({}, { all = true, max_count = 5000 })
+      end, {})
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "gitgraph",
+        callback = function(ev)
+          vim.keymap.set("n", ".", function()
+            local row = vim.api.nvim_win_get_cursor(0)[1]
+            local line = vim.api.nvim_get_current_line()
+            local target
+            if row % 2 == 0 then
+              line = vim.api.nvim_buf_get_lines(0, row - 2, row - 1, false)[1]
+              target = line:match("%x%x%x%x%x%x%x+")
+            else
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local paren = line:match(".*(%b())")
+              local ps = paren and line:find(paren, 1, true)
+              local in_parens = ps and col >= ps - 1 and col <= ps - 2 + #paren
+              target = in_parens and vim.fn.expand("<cWORD>"):gsub("[()]", "") or line:match("%x%x%x%x%x%x%x+")
+            end
+            if target and target ~= "" then
+              local keys = vim.api.nvim_replace_termcodes(": " .. target .. "<Home>Git ", true, false, true)
+              vim.api.nvim_feedkeys(keys, "n", false)
+            end
+          end, { buffer = ev.buf, desc = "Git command with ref" })
+        end,
+      })
+    end,
   },
   {
     "gitusp/gh-utils.nvim",
