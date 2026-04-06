c167ae40ef2721cc36cf4fcc66d684155e720976
usp <ususso@gmail.com>
2026-04-06 23:08:47 +0900

    feat: adjust terminal behavior

--- .config/nvim/lua/plugins/claudecode.lua
+++ .config/nvim/lua/plugins/claudecode.lua
@@ -1,7 +1,26 @@
 return {
   "coder/claudecode.nvim",
   dependencies = { "folke/snacks.nvim" },
-  config = true,
+  opts = {
+    terminal = {
+      snacks_win_opts = {
+        keys = {
+          term_normal = false,
+        },
+        on_buf = function(self)
+          -- Disable the auto_insert BufEnter autocmd that snacks.terminal sets up.
+          -- Deferred because the autocmd is registered after Snacks.win() returns.
+          vim.schedule(function()
+            vim.api.nvim_clear_autocmds({
+              event = "BufEnter",
+              group = self.augroup,
+              buffer = self.buf,
+            })
+          end)
+        end,
+      },
+    },
+  },
   keys = {
     { "<c-\\>", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
     { "<c-'>", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
